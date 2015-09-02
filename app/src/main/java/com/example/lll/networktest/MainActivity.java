949fcf70aacee0d04ab8078347997eda13e0e305
Nathaniel <linlilulll@gmail.com>
2015-09-02 20:46:09 +0800

    Using HttpClient to do the same job

--- app/src/main/java/com/example/lll/networktest/MainActivity.java
+++ app/src/main/java/com/example/lll/networktest/MainActivity.java
@@ -9,18 +9,18 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-import java.io.BufferedReader;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
     public static final int SHOW_RESPONSE = 0;
     private Button sendRequest;
     private TextView responseText;
     private Handler handler = new Handler() {
-
         public void handleMessage(Message msg) {
             switch (msg.what){
                 case SHOW_RESPONSE:
@@ -44,39 +44,29 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public void onClick(View v) {
         if(v.getId() == R.id.send_request) {
-            sendRequestWithHttpURLConnection();
+            sendRequestWithHttpClient();
         }
     }
 
-    private void sendRequestWithHttpURLConnection() {
+    private void sendRequestWithHttpClient() {
         new Thread(new Runnable() {
             @Override
             public void run() {
-                HttpURLConnection connection = null;
-                try{
-                    URL url = new URL("http://www.baidu.com");
-                    connection = (HttpURLConnection) url.openConnection();
-                    connection.setRequestMethod("GET");
-                    connection.setConnectTimeout(8000);
-                    connection.setReadTimeout(8000);
-                    InputStream in = connection.getInputStream();
-                    BufferedReader reader = new BufferedReader(new
-                            InputStreamReader(in));
-                    StringBuilder response = new StringBuilder();
-                    String line;
-                    while ((line = reader.readLine()) != null) {
-                        response.append(line);
+                try {
+                    HttpClient httpClient = new DefaultHttpClient();
+                    HttpGet httpGet = new HttpGet("http://www.baidu.com");
+                    HttpResponse httpResponse = httpClient.execute(httpGet);
+                    if(httpResponse.getStatusLine().getStatusCode() == 200) {
+                        HttpEntity entity = httpResponse.getEntity();
+                        String response = EntityUtils.toString(entity, "utf-8");
+                        Message message = new Message();
+                        message.what = SHOW_RESPONSE;
+                        message.obj = response.toString();
+                        handler.sendMessage(message);
                     }
-                    Message message = new Message();
-                    message.what = SHOW_RESPONSE;
-                    message.obj = response.toString();
-                    handler.sendMessage(message);
+
                 }catch (Exception e) {
                     e.printStackTrace();
-                } finally {
-                    if(connection != null) {
-                        connection.disconnect();
-                    }
                 }
             }
         }).start();
